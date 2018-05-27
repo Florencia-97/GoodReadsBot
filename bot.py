e6243989b187ad7cff9b6dd065c8aa5285def3a6
Flox <florrr1997@gmail.com>
2018-05-27 12:30:42 -0300

    agrego descripición y link

--- bot.py
+++ bot.py
@@ -25,19 +25,35 @@ def search(message):
 	libro = ""
 	for palabra in nombre_ingresado:
 		libro += palabra + '+'
-	titulo, score, imagen = goodreads(libro[:-1].encode('utf-8').strip())
-	bot.reply_to(message, titulo + '\n' + score + '\n' + imagen)
-
+	titulo, score, imagen, review, link = goodreads(libro[:-1].encode('utf-8').strip())
+	bot.reply_to(message, titulo + '\n' + score + '\n' + 'Cover: ' + imagen + '\n' + 'Link: ' + link + '\n' + 'Description: ' + review[:300] + '...')
 
 def goodreads(libro):
 	r = requests.get("https://www.goodreads.com/search.xml?key=LcLFw9NWm11xXMzNkBDHUQ&q={}".format(str(libro)))
 	try:
-		title = xmltodict.parse(r.content)['GoodreadsResponse']['search']['results']['work'][0]['best_book']['title']
+		book = xmltodict.parse(r.content)['GoodreadsResponse']['search']['results']['work'][0]
 	except:
 		print("hola")
-	score = xmltodict.parse(r.content)['GoodreadsResponse']['search']['results']['work'][0]['average_rating']
-	imagen = xmltodict.parse(r.content)['GoodreadsResponse']['search']['results']['work'][0]['best_book']['image_url']
-	return title, score, imagen
+	title = book['best_book']['title']
+	score = book['average_rating']
+	imagen = book['best_book']['image_url']
+	id = book['best_book']['id']['#text']
+	review, link =  get_review_and_link(id)
+	return title, score, imagen, review, link
 
+def get_review_and_link(libro_id):
+	r = requests.get("https://www.goodreads.com/book/show/{}.xml?key=LcLFw9NWm11xXMzNkBDHUQ".format(libro_id))
+	book = xmltodict.parse(r.content)['GoodreadsResponse']['book']
+	descripcion = book['description']
+	url = book['url']
+	descripcion = filtrar_descripcion(descripcion)
+	return descripcion, url
+
+def filtrar_descripcion(descripcion):
+	corte = '<br />'
+	for i in range(2):
+		comienzo = descripcion.index(corte) + len(corte)
+		descripcion = descripcion[comienzo:]
+	return descripcion
 
 bot.polling()
